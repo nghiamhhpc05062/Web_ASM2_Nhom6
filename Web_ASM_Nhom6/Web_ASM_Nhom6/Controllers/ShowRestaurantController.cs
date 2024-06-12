2abf04ffebed4182a0aa09ddcddcfe808f14f266
triet124 <trantantriet18112004@gmail.com>
2024-06-12 08:50:48 +0700

    Đăng ký, Liên hệ, Cửa hàng v2
    Tách Restaurant ra khỏi home

---
+++ Web_ASM_Nhom6/Web_ASM_Nhom6/Controllers/ShowRestaurantController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Web_ASM_Nhom6.Models;
+
+namespace Web_ASM_Nhom6.Controllers
+{
+	public class ShowRestaurantController : Controller
+	{
+		public string url2 = "http://localhost:29015/api/Restaurant";
+		public async Task<IActionResult> ShowRestaurant(int id)
+		{
+			Restaurant restaurant = new Restaurant();
+			using (var httpclient = new HttpClient())
+			{
+				using (var response = await httpclient.GetAsync($"{url2}/{id}"))
+				{
+					string apiResponse = await response.Content.ReadAsStringAsync();
+					restaurant = JsonConvert.DeserializeObject<Restaurant>(apiResponse);
+				}
+			};
+			return View(restaurant);
+		}
+	}
+}
